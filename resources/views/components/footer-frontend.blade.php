b318012481afd7989408d49ca49ddfb24115350a
widaaryanti <widaaryanti13@gmail.com>
2024-07-22 07:15:18 +0700

    update data

--- resources/views/components/footer-frontend.blade.php
+++ resources/views/components/footer-frontend.blade.php
@@ -30,7 +30,7 @@
             </div>
             <div class="col-lg-4 mb-3">
                 <h5 class="fw-bold text-ajl-secondary mb-3">Map</h5>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.2301657818502!2d108.22444907412564!3d-7.3280259926802795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f575ac796e5bd%3A0x1266496f1d655684!2sUniversitas%20BSI%20Kampus%20Tasikmalaya!5e0!3m2!1sid!2sid!4v1721540388121!5m2!1sid!2sid"  width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d989.4120021543815!2d108.20866526951015!3d-7.2808309995454055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMTYnNTEuMCJTIDEwOMKwMTInMzMuNSJF!5e0!3m2!1sid!2sid!4v1721607076475!5m2!1sid!2sid" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></>
             </div>
         </div>
     </div>
